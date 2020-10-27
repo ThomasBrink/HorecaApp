8bfb21f4860fc930bab6273a157008257a51ca41
Thomas <99062905@mydavinci.nl>
2020-10-27 12:54:47 +0100

    klaar voor vandaag

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
-	var bestelling
+	var bestelling 
+	var linebreak = "<br>";
+	var totaal
 
-        function WelkDrankje(){
-
-        	bestelling = prompt ("Welke bestelling wilt u toevoegen? U kunt kiezen uit fris 1,-, bier 2,- of wijn 3,-");
-
-            if(bestelling == "fris" || bestelling == "bier" || bestelling == "wijn"){
-                var hoeveelfris = prompt ("hoeveel "+ bestelling + " wilt u bestellen?");
-                var antfris = hoeveelfris * 1
-            }
-
-            else{
-            	alert ("U heeft een ongeldige invoer gedaan.")
-                WelkDrankje();
-
-            }
+		function som(){
+        	totaal = antfris + antbier + antwijn
         }
+ 
+        function WelkDrankje(){ 
+ 
+        	bestelling = prompt ("Welke bestelling wilt u toevoegen? U kunt kiezen uit fris 1,-, bier 2,- of wijn 3,-"); 
+ 
+            if(bestelling == "fris"){ 
+                var hoeveelfris = prompt ("hoeveel fris wilt u bestellen?"); 
+                var antfris = hoeveelfris * 1 
+                document.write("U heeft " + antfris + ",- aan fris")
+                document.write(linebreak);
+                WelkDrankje();
+            } 
+ 
+            else if(bestelling == "bier"){ 
+                var hoeveelbier = prompt ("Hoeveel bier wilt u bestellen?"); 
+                var antbier = hoeveelbier * 2
+                document.write("U heeft " + antbier + ",- aan bier")
+                document.write(linebreak);
+                WelkDrankje();
+            } 
+            else if(bestelling == "wijn"){ 
+                var hoeveelwijn = prompt ("Hoeveel wijn wilt u bestellen?"); 
+                var antwijn = hoeveelwijn * 3
+                document.write("U heeft " + antwijn + ",- aan wijn")
+                document.write(linebreak);
+                WelkDrankje();
+            } 
 
+            else if(bestelling == "stop"){
+            	som();
+            	document.write("Uw totaal bedrag is " + totaal +",-")
+            }
+
+            else{ 
+            	alert ("U heeft een ongeldige invoer gedaan.") 
+                WelkDrankje(); 
+ 
+            }
+
+        } 
+
+ 
         WelkDrankje();
