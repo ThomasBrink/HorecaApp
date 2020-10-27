93841303fe405264d19c23394e131e63f7c7ad82
Thomas <99062905@mydavinci.nl>
2020-10-27 12:31:39 +0100

    nope

--- script.js
+++ script.js
@@ -4,17 +4,11 @@
 
         	bestelling = prompt ("Welke bestelling wilt u toevoegen? U kunt kiezen uit fris 1,-, bier 2,- of wijn 3,-");
 
-            if(bestelling == "fris"){
-                var hoeveelfris = prompt ("hoeveel fris wilt u bestellen?");
+            if(bestelling == "fris" || bestelling == "bier" || bestelling == "wijn"){
+                var hoeveelfris = prompt ("hoeveel "+ bestelling + " wilt u bestellen?");
                 var antfris = hoeveelfris * 1
             }
 
-            else if(bestelling == "bier"){
-                var hoeveelbier = prompt ("Hoeveel bier wilt u bestellen?");
-            }
-            else if(bestelling == "wijn"){
-                var hoeveelwijn = prompt ("Hoeveel wijn wilt u bestellen?");
-            }
             else{
             	alert ("U heeft een ongeldige invoer gedaan.")
                 WelkDrankje();
