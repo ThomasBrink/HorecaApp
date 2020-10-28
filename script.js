9efdafeaf408710cd73d825917cfffd96eb93167
Thomas <99062905@mydavinci.nl>
2020-10-28 11:51:52 +0100

    NaN bug gefixt

--- script.js
+++ script.js
@@ -7,6 +7,7 @@
 	var bitterbal
 	var antacht
 	var antzestien
+	var totaalbitter
 
  
         function WelkDrankje(){ 
@@ -15,49 +16,53 @@
  
             if(bestelling == "fris"){ 
                 var hoeveelfris = prompt ("hoeveel fris wilt u bestellen?"); 
-                antfris = hoeveelfris * 1 
-                if(hoeveelfris != null){
-	                antfris = 0;
-	            	WelkDrankje();
-	            }
-	            else{
-	            	document.write("U heeft " + antfris + ",- aan fris");
-	                document.write(linebreak);
-	                WelkDrankje();	            	
-	            }
+                	antfris = hoeveelfris * 1 
+		            document.write("U heeft " + antfris + ",- aan fris");
+		            document.write(linebreak);
+		            WelkDrankje();         	
             } 
  
             else if(bestelling == "bier"){ 
                 var hoeveelbier = prompt ("Hoeveel bier wilt u bestellen?"); 
                 antbier = hoeveelbier * 2
-                if(hoeveelbier != null){
-	                antbier = 0;
-	            	WelkDrankje();
-	            }
-	            else{
-	            	document.write("U heeft " + antbier + ",- aan bier");
-	                document.write(linebreak);
-	                WelkDrankje();	            	
-	            }
+	            document.write("U heeft " + antbier + ",- aan bier");
+	            document.write(linebreak);
+	            WelkDrankje();	            	
             }
 
             else if(bestelling == "wijn"){ 
                 var hoeveelwijn = prompt ("Hoeveel wijn wilt u bestellen?"); 
                 antwijn = hoeveelwijn * 3
-                if(hoeveelwijn != null){
-	                antbier = 0;
-	            	WelkDrankje();
-	            }
-	            else{
-	            	document.write("U heeft " + antwijn + ",- aan wijn");
-	                document.write(linebreak);
-	                WelkDrankje();
-	            }
+	            document.write("U heeft " + antwijn + ",- aan wijn");
+	            document.write(linebreak);
+	            WelkDrankje();
+
             } 
 
             else if(bestelling == "stop"){
+
+            	while(antfris == null){
+            		antfris = 0;
+            	}
+
+            	while(antbier == null){
+            		antbier = 0;
+            	}
+
+            	while(antwijn == null){
+            		antwijn = 0;
+            	}
+
+            	while(antacht == null){
+            		antacht = 0;
+            	}
+
+            	while(antzestien == null){
+            		antzestien = 0;
+            	}
+
             	totaal = antfris + antbier + antwijn + antacht + antzestien
-            	document.write("Uw totaal bedrag is " + totaal +",-");
+            	document.write("Uw totaal bedrag is " + totaal +",- aan drankjes");
             }
 
             else if(bestelling == "snack"){
@@ -78,36 +83,24 @@
 	            		if(bitterbal == "8"){
 	            			var hoeveelacht = prompt ("Hoeveel bitterbalschalen van 8 wilt u bestellen?");
                             antacht = hoeveelacht * 4
-                            if(hoeveelacht != null){
-	                            antacht = 0;
-	                        	WelkDrankje();
-	                        }
-	                        else{
-	                        	document.write("U heeft " + antacht +",- aan bitterbalschalen (8) besteed");
-	                            document.write(linebreak);
-	                            WelkDrankje();
-	                        }
+	                        document.write("U heeft " + antacht +",- aan bitterbalschalen (8) besteed");
+	                        document.write(linebreak);
+	                        WelkDrankje();
 	            		}
 
 	            		else if(bitterbal == "16"){
 	            			var hoeveelzestien = prompt ("Hoeveel bitterbalschalen van 16 wilt u bestellen?");
                             antzestien = hoeveelzestien * 8
-                            if(hoeveelzestien != null){
-	                            antzestien = 0;
-	                        	WelkDrankje();
-	                        }
-	                        else{
-	                        	document.write("U heeft " + antzestien + ",- aan bitterbalschalen (16) besteed");
-	                            document.write(linebreak);
-	                            WelkDrankje();	          
-	                        }
+	                        document.write("U heeft " + antzestien + ",- aan bitterbalschalen (16) besteed");
+	                      	document.write(linebreak);
+	                        WelkDrankje();
 	            		}
 
 	            		else{
 	            			alert ("Dit is een ongeldige invoer probeer opnieuw.");
 	            			bitterbestel();
 	            		}
-	        	}
 
+	        	}
  
         WelkDrankje();
