f22ad9fb0f28eecfc2279f17213d110717562147
Thomas <99062905@mydavinci.nl>
2020-10-28 07:58:47 +0100

    totaal bedrag som gefixt

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 	var bestelling 
 	var linebreak = "<br>";
 	var totaal
+	var antfris
+	var antbier
+	var antwijn
 
-		function som(){
-        	totaal = antfris + antbier + antwijn
-        }
  
         function WelkDrankje(){ 
  
@@ -12,7 +12,7 @@
  
             if(bestelling == "fris"){ 
                 var hoeveelfris = prompt ("hoeveel fris wilt u bestellen?"); 
-                var antfris = hoeveelfris * 1 
+                antfris = hoeveelfris * 1 
                 document.write("U heeft " + antfris + ",- aan fris")
                 document.write(linebreak);
                 WelkDrankje();
@@ -20,21 +20,21 @@
  
             else if(bestelling == "bier"){ 
                 var hoeveelbier = prompt ("Hoeveel bier wilt u bestellen?"); 
-                var antbier = hoeveelbier * 2
+                antbier = hoeveelbier * 2
                 document.write("U heeft " + antbier + ",- aan bier")
                 document.write(linebreak);
                 WelkDrankje();
             } 
             else if(bestelling == "wijn"){ 
                 var hoeveelwijn = prompt ("Hoeveel wijn wilt u bestellen?"); 
-                var antwijn = hoeveelwijn * 3
+                antwijn = hoeveelwijn * 3
                 document.write("U heeft " + antwijn + ",- aan wijn")
                 document.write(linebreak);
                 WelkDrankje();
             } 
 
             else if(bestelling == "stop"){
-            	som();
+            	totaal = antfris + antbier + antwijn
             	document.write("Uw totaal bedrag is " + totaal +",-")
             }
 
